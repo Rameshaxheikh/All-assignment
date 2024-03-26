c119ec8112342b68fecee576863f4dd46950084c
Lenovo <techmarketingagancy@gmail.com>
2024-03-26 12:21:39 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import chalkAnimation from 'chalk-animation';
+import chalk from  'chalk';
+ const rainbow = chalkAnimation.rainbow('TRY CLC  GAME ');
+setTimeout(() => {
+    rainbow.stop(); 
+}, 1000);
+setTimeout(() => {
+    rainbow.start(); 
+}, 3000);
+import inquirer from "inquirer";
+let number_generate = Math.floor (Math.random() * 5 +1);
+const answer1  = await inquirer.prompt([
+{
+    name : "user_guest",
+    type : "number",
+    message :"write your guest_number  between 1 to 10",
+}
+])
+const {user_guest} = answer1;
+console.log(`${user_guest} , your guess ,${number_generate} ,system generate number`)
+if (user_guest == number_generate) {
+    console.log(chalk.blue("your answer is correct , your WIN GAME"));
+}
+else{
+    console.log(chalk.green("TRY AGAIN"))
+;
+}
